0b69a85400d75867ae4e30e8d3d9b1e0d6deca50
Aluno Lab 135 <lab135@lab.senai>
2023-08-25 21:16:34 -0300

    Calcula função soma e subtração

--- calculadora.py
+++ calculadora.py
@@ -1,4 +1,10 @@
 print("---------CALCULADORA---------")
+
+n=int(input("Digite 1 para soma, 2 para subtração:"))
 x=float(input("Digite o primeiro número: "))
 y=float(input("Digite o segundo número: "))
-print(x+y)
+
+if n==1:
+ print(x+y)
+if n==2:
+ print(x-y)
