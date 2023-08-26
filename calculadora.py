560987ed001410335aac3a343bcf471ec151f6f9
Aluno Lab 135 <lab135@lab.senai>
2023-08-25 22:02:57 -0300

    Calcula função soma,subtração,multiplicação,divisão

--- calculadora.py
+++ calculadora.py
@@ -1,10 +1,17 @@
 print("---------CALCULADORA---------")
 
-n=int(input("Digite 1 para soma, 2 para subtração:"))
+n=int(input("Digite 1 para soma, 2 para subtração,3 para multiplicação,4 para divisão:"))
 x=float(input("Digite o primeiro número: "))
 y=float(input("Digite o segundo número: "))
 
 if n==1:
- print(x+y)
+ print("RESULTADO:",x+y)
 if n==2:
- print(x-y)
+ print("RESULTADO:",x-y)
+if n==3:
+ print("RESULTADO:",x*y)
+if n==4:
+  if y==0:
+   print("Não existe divisão por zero!")
+  else:
+    print("RESULTADO:",x/y) 
